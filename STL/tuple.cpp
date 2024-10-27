5f8f2b93c1bc03dcaafbd4a5a5f81c6753014d23
Abdullah Al Noman <165512794+noman797@users.noreply.github.com>
2024-10-27 12:48:01 +0600

    Add files via upload
    Standard Template Library

---
+++ STL/tuple.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);   
+    cin.tie(0);
+
+    // tuple<string,int,string>t=make_tuple("rahim",10,"017");
+    // cout<<get<0>(t)<<" "<<get<1>(t)<<" "<<get<2>(t)<<endl;
+
+    return 0;
+}
